9c825b48d361274bd17332b81a864f778b97335b
Miles Ren <yren9582@uni.sydney.edu.au>
2023-09-19 08:36:34 +1000

    Add Profile Backend

---
+++ backendcloud/backend/src/main/java/com/kob/backend/controller/user/account/UserProfileController.java
@@ -0,0 +1,42 @@
+package com.kob.backend.controller.user.account;
+
+import com.kob.backend.pojo.Bot;
+import com.kob.backend.pojo.snakeRecord;
+import com.kob.backend.service.user.account.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/user/account")
+public class UserProfileController {
+    @Autowired
+    private UserProfileService userProfileService;
+
+    // Get user information
+    @GetMapping("/info")
+    public Map<String, String> getInfo(){
+        return userProfileService.getInfo();
+    }
+
+    // Update user information
+    @PostMapping("/update")
+    public Map<String, String> updateInfo(@RequestBody Map<String, String> data){
+        return userProfileService.updateInfo(data);
+    }
+
+    // Get list of robots
+    @GetMapping("/bots")
+    public List<Bot> getBots() {
+        return userProfileService.getBots();
+    }
+
+    // Get list of game records
+    @GetMapping("/records")
+    public List<snakeRecord> getRecords() {
+        return userProfileService.getRecords();
+    }
+}
